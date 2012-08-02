e663058515e199d85d8b426a29cbdd8a1da10cbb
jun <jun@jun-ThinkPad-SL400.(none)>
2012-08-02 23:04:41 +0800

    driver_programming.c: add contents

--- driver_programming.c
+++ driver_programming.c
@@ -8,7 +8,7 @@
 	};
 
     struct class_attribute jun_class_attrs = {
-		__ATTR(jun_file, 0644, jun_file_show, jun_file_store), // will have "/sys/class/jun_file" file
+		__ATTR(jun_file, 0644, jun_file_show, jun_file_store), // will have "/sys/class/jun/jun_file" file
 		...
 		__ATTR_NULL,
 	};
@@ -17,3 +17,22 @@
 	ssize_t jun_file_store(struct class *class, struct class_attribute *attr, const char *buf, size_t count) {...}
 
 
+
+2. /sys/devices interface
+
+    error = sysfs_create_group(&client->dev.kobj, &jun_attr_group); //error == 0:successful. if client is i2c's device,
+																	//then it will have "/sys/devices/i2c-x/x-<addr>/jun"
+																    //file.
+	struct attribute_group jun_attr_group = {                       //
+		.name = NULL, 
+		.attrs = jun_attr,
+	};
+
+	static DEVICE_ATTR(jun, S_IRWXUGO, jun_read, jun_write);
+	static struct attribute* jun_attr[] = {
+	       &dev_attr_jun.attr,
+	       NULL
+	};
+
+    ssize_t jun_read(struct device *dev, struct device_attribute *attr, char *buf){...}
+	ssize_t jun_write(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
